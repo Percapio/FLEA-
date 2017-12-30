06c25863828870cb3100c6d0babd685b8739f90d
Thomas Vu <mr.thomas.vu@gmail.com>
2017-12-30 00:47:35 -0800

    added art, music and recalibrated movement for player and enemies

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
-
 module.exports = {
   entry: "./lib/gate_is_down.js",
   output: {
     filename: "./lib/bundle.js"
   },
   devtool: 'source-map',
+  module: {
+  	loaders: [
+  		{
+  			test: /\.json$/,
+  			loader: 'json-loader'
+  		}
+		]
+	},
+  // alias: {
+  //   'pg-native': path.join(__dirname, 'aliases/pg-native.js'),
+  //   'pgpass$': path.join(__dirname, 'aliases/pgpass.js'),
+  // },
+	node: {
+		fs: 'empty',
+		net: 'mock',
+		tls: 'mock',
+    dns: 'mock'
+	},
+	// target: 'node'
 };
