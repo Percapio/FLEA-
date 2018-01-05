c420106538a565d1d725e25c0bda36de57098137
Thomas Vu <mr.thomas.vu@gmail.com>
2018-01-05 13:38:59 -0800

    Fixed canvases rendering issues & relocated game files

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express     = require('express'),
       logger      = require('morgan'),
-      bodyParser  = require('body-parser');
+      bodyParser  = require('body-parser'),
+      path        = require('path');
 const app         = express();
 
 app.use( logger('dev' ));
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'lib')));
 
 require('./server/routes')(app);
 app.get('*', (req, res) => res.status(200).send({
--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,12 @@ module.exports = {
     filename: "./lib/bundle.js"
   },
   devtool: 'source-map',
-  module: {
-  	loaders: [
-  		{
-  			test: /\.json$/,
-  			loader: 'json-loader'
-  		}
-		]
-	},
-  // alias: {
-  //   'pg-native': path.join(__dirname, 'aliases/pg-native.js'),
-  //   'pgpass$': path.join(__dirname, 'aliases/pgpass.js'),
-  // },
-	node: {
-		fs: 'empty',
-		net: 'mock',
-		tls: 'mock',
-    dns: 'mock'
-	},
-	// target: 'node'
+  // module: {
+  // 	loaders: [
+  // 		{
+  // 			test: /\.json$/,
+  // 			loader: 'json-loader'
+  // 		}
+	// 	]
+	// },
 };
